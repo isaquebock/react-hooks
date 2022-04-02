f890b49f67ec5192489af6dcadc8e74c62906e2f
Isaque Böck <isaquebockdos@gmail.com>
2022-04-01 23:15:04 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0)
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect ativo')
+
+    return () => {
+      console.log('vai sair da tela')
+    }
+  }, [])
 
   const handleMinus = () => {
     setCounter(prevState => prevState - 1)
@@ -20,6 +28,9 @@ function App() {
 
         <button onClick={handlePlus}>+</button>
         <button onClick={handleMinus}>-</button>
+        
+        {name}
+        <input type="text" onChange={e => setName(e.target.value)} />
       </header>
     </div>
   );
