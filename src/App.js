6c382526e636ee0222a2c86150d615d8a69ba6ab
Isaque Böck <isaquebockdos@gmail.com>
2022-04-07 12:50:08 -0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createContext, useContext } from 'react';
+
+const ThemeContext = createContext()
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [name, setName] = useState('')
-
-  useEffect(() => {
-    console.log('useEffect ativo')
-
-    return () => {
-      console.log('vai sair da tela')
+  return (
+    <ThemeContext.Provider 
+    value={
+      {
+        mode: 'dark'
+      }
     }
-  }, [])
+    >
+        <Button />
+    </ThemeContext.Provider>
+  );
+}
 
-  const handleMinus = () => {
-    setCounter(prevState => prevState - 1)
-  }
-
-  const handlePlus = () => {
-    setCounter(prevState => prevState + 1)
-  }
+function Button() {
+  const theme = useContext(ThemeContext);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {counter}
-
-        <button onClick={handlePlus}>+</button>
-        <button onClick={handleMinus}>-</button>
-        
-        {name}
-        <input type="text" onChange={e => setName(e.target.value)} />
-      </header>
-    </div>
-  );
+    <button>{theme.mode}</button>
+  )
 }
 
 export default App;
