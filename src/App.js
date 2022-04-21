39af0946bb9092b38fb3ec4717094bd0dd165e44
Isaque Böck <isaquebockdos@gmail.com>
2022-04-21 15:43:22 -0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import React, { useState, useCallback } from 'react';
+import React, { useState, useMemo } from 'react';
 
 function App() {
-
+  const [name, setName] = useState('')
   const [counter, setCounter] = useState(0)
 
-  const handlePlus = useCallback(() => {
-    setCounter((prevState) => prevState + 1)
-  }, [])
+  const total = useMemo(() => {
+    return counter * 10 / 2
+  }, [counter])
+
+  const handlePlus = () => {
+    setCounter((prevState) => prevState + 1);
+  }
 
   return (
     <div>
       <h1>{counter}</h1>
       <button onClick={handlePlus}>+</button>
+
+      <h2>Resultado: {total}</h2>
+
+      <br /><br />
+
+      <strong>{ name }</strong><br />
+      <input onChange={e => setName(e.target.value)} />
     </div>
   );
 }
